dc722f51a7fb8622668be9d795b32c88dec5e8a4
= <=>
2026-09-17 22:55:15 +0530

    Initial commit

---
+++ Studentdemo3/src/test/java/com/example/Studentdemo3/Studentdemo3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Studentdemo3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Studentdemo3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
